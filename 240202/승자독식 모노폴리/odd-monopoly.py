242e73ceb987063bd80828750eb86b505e63da9b
Heavensbee <65532083+beeheavens@users.noreply.github.com>
2024-02-02 22:39:12 +0900

    Title: 승자독식 모노폴리, Time: 304ms, Memory: 27MB, Status: Passed - CodeTree

--- 240202/승자독식 모노폴리/odd-monopoly.py
+++ 240202/승자독식 모노폴리/odd-monopoly.py
@@ -1,14 +1,19 @@
-#1.이동
-#2.제거
-#3.독점계약
-#4.턴--
-
 def move():
+    global player_num
+    """for i in player_map:
+        print(i)
+    print("-----")
+    print(player_data)
+    print("---")
+    for i in contract_map:
+        print(i)"""
     for idx, player in enumerate(player_data): #player data에서 이동 처리
         y = player[0]
         x = player[1]
         move = 0 #이동 여부 판단 flag
         #1. 미계약 땅 확인 
+        if(y == -100):
+            continue
         for i in range(4): 
             next_y = y+dy[dir_data[idx][player[2]-1][i]-1]#아 데이터에 플레이어 번호를 넣는게 편할거같다.. 안넣으니까 자꾸 index를 찾게돼 -> enumerate를 쓰면 된다!!!
             next_x = x+dx[dir_data[idx][player[2]-1][i]-1]
@@ -16,20 +21,24 @@ def move():
                 if(contract_map[next_y][next_x][0]==0): #미계약 땅
                     player[0] = next_y
                     player[1] = next_x
+                    player[2] = dir_data[idx][player[2]-1][i]
                     move = 1
                     break
                     #계약을 지금 하는게 아님
         if(move == 1): #움직였는지 확인을 해야함    
             continue #움직였으면 다음 사람 차례
         for i in range(4): #안움직인 경우, 본인 땅으로 가야함
-            next_y = y+dy[dir_data[idx][player[2]-1][i]]#아 데이터에 플레이어 번호를 넣는게 편할거같다.. 안넣으니까 자꾸 index를 찾게돼 -> enumerate를 쓰면 된다!!!
-            next_x = x+dx[dir_data[idx][player[2]-1][i]]
-            if(contract_map[next_y][next_x][0]==idx): #미계약 땅
-                player[0] = next_y
-                player[1] = next_x
-                move = 1
-                break
+            next_y = y+dy[dir_data[idx][player[2]-1][i]-1]#아 데이터에 플레이어 번호를 넣는게 편할거같다.. 안넣으니까 자꾸 index를 찾게돼 -> enumerate를 쓰면 된다!!!
+            next_x = x+dx[dir_data[idx][player[2]-1][i]-1]
+            if(0<=next_y<map_size and 0<=next_x<map_size):
+                if(contract_map[next_y][next_x][0]==idx+1): #본인 땅
+                    player[0] = next_y
+                    player[1] = next_x
+                    player[2] = dir_data[idx][player[2]-1][i]
+                    move = 1
+                    break
     #이제 player map에서 사람을 움직여야 함.
+
     for i in range(map_size):
         for j in range(map_size):
             player_map[i][j] = 0 #일단 맵을 전부 0으로 초기화
@@ -37,12 +46,24 @@ def move():
     for idx, data in enumerate(player_data):
         i = data[0]
         j = data[1]
+        if(i == -100):
+            continue
         if(player_map[i][j]==0):
             player_map[i][j] = idx+1 #이동 !!!!이럴수가. idx로 넣으니까, 빈칸을 의미하는 0과 플레이어 1번의 idx인 0이 구분이 안된다;
         elif(player_map[i][j]!= 0): #누가 있는 경우
-            del_list.append(idx) #없어져야 하는 플레이어 번호 추가
+            
+            data[0] = -100
+            player_num -= 1
+            #del_list.append(idx) #없어져야 하는 플레이어 번호 추가
+    
+    #for i in player_map:
+    #    print(i)
+    #print("-----")
+    #print(player_data)
+    #print("---")
+    """ 삭제가 완전히 틀렸네..
     for i in range(len(del_list)): #플레이어를 데이터에서 삭제
-        del player_data[del_list[i]-i]
+        del player_data[del_list[i]-i] """
 
 def turn():
     for i in range(map_size):
@@ -54,16 +75,20 @@ def turn():
 
 def contract():
     for idx, data in enumerate(player_data):
-        contract_map[data[0]][data[1]][0] = idx
+        if(data[0]==-100):
+            continue
+        contract_map[data[0]][data[1]][0] = idx+1
         contract_map[data[0]][data[1]][1] = turn_num
 
 def check():
-    if (len(player_data)==1):
+    global player_num
+    if (player_num==1):
         return False
-    elif(len(player_data)>1):
+    elif(player_num>1):
         return True
 
 ####initialize####
+global player_num
 map_size, player_num, turn_num = map(int,input().split())
 player_map = []
 contract_map = []
@@ -86,6 +111,8 @@ for i in range(map_size):
             player_data[player_map[i][j]-1].insert(0,j)
             player_data[player_map[i][j]-1].insert(0,i)
             
+
+
 for i in range(player_num): #dir_data 생성
     temp = []
     for j in range(4):
@@ -98,7 +125,7 @@ contract_map = [[[0,0]for i in range(map_size)]for j in range(map_size)] #contra
 for data in range(len(player_data)): #이거 좀 불편한데, len 안 쓰고 index를 얻는 방법이 없을까? --> enumerate를 쓰면 된다!!
     y = player_data[data][0]
     x = player_data[data][1]
-    contract_map[y][x][0] = data
+    contract_map[y][x][0] = data+1
     contract_map[y][x][1] = turn_num # contract_map init
 
 
@@ -112,4 +139,5 @@ while(check()):
     if(ans>1000):
         ans = -1
         break
+    
 print(ans)
