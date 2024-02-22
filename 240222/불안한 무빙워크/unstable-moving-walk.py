e6a6e7da8525c26374f882d8abe135b70eb102a5
Heavensbee <65532083+beeheavens@users.noreply.github.com>
2024-02-22 22:04:53 +0900

    Title: 불안한 무빙워크, Time: 444ms, Memory: 28MB, Status: Passed - CodeTree

--- 240222/불안한 무빙워크/unstable-moving-walk.py
+++ 240222/불안한 무빙워크/unstable-moving-walk.py
@@ -2,54 +2,38 @@ def move_moving_walk():
     temp = moving_walk[len(moving_walk)-1]
     del moving_walk[len(moving_walk)-1]
     moving_walk.insert(0,temp)
-    for idx,value in enumerate(person_data):
-        person_data[idx] += 1
-    try:
-        if(person_data[0]==length):
-            moving_walk[person_data[0]][1] = 0
-            del person_data[0]
-    except:
-        pass
-
+    if(moving_walk[length-1][0]==1):
+        moving_walk[length-1][0] = 0
+    
 def move_person():
-    for idx, value in enumerate(person_data):
-        if(moving_walk[value+1][0]!=0 and moving_walk[value+1][1]==0):
-            index = value + 1
-            moving_walk[value][1] = 0
-            person_data[idx] += 1
-            moving_walk[index][1] = 1
-            moving_walk[index][0] -= 1
-    try:
-        if(person_data[0] == length):
-            moving_walk[person_data[0]][1] = 0
-            del person_data[0]
-    except:
-        pass
+    for i in range(length-1,-1,-1):
+        if(moving_walk[i][0]==1):
+            if(moving_walk[i+1][0]==0 and moving_walk[i+1][1]>0):
+                moving_walk[i][0] = 0
+                moving_walk[i+1][0] = 1
+                moving_walk[i+1][1] -= 1
+                if(i+1 == length-1 and moving_walk[i+1][0] == 1):
+                    moving_walk[i+1][0] = 0
 
 def add_person():
-    if(moving_walk[0][0]!=0 and moving_walk[0][1]==0):
-        moving_walk[0][1] = 1
-        moving_walk[0][0] -= 1
-        person_data.append(0)
+    if(moving_walk[0][0] == 0 and moving_walk[0][1]>0):
+        moving_walk[0][0] = 1
+        moving_walk[0][1] -= 1
 
 def check():
-    num = 0
+    checker = 0
     for i in moving_walk:
-        if(i[0]==0):
-            num += 1
-    if(num>=finish_num):
-        return True
-    else:
-        return False
+        if(i[1] == 0):
+            checker += 1
+    return checker
+                
+                
 
 ### main ###
 length, finish_num = map(int,input().split())
-moving_walk = list(map(list,input().split()))
-for i in moving_walk:
-    i[0] = int(i[0])
-    i.append(0)
-global person_data
-person_data = []
+moving_walk = list(map(int,input().split()))
+for idx,value in enumerate(moving_walk):
+    moving_walk[idx] = [0,value]
 
 ans = 0
 while(1):
@@ -57,8 +41,6 @@ while(1):
     move_moving_walk()
     move_person()
     add_person()
-    if(check()):
+    if(check() >= finish_num):
         break
-    
-
 print(ans)
